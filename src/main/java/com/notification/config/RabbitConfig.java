df3a44ab656b103f5330b40f6e7c49ff12f85021
Mad-iq <debashritamandal852@gmail.com>
2025-12-01 13:03:59 +0530

    Added the config for rabbitMq

---
+++ src/main/java/com/notification/config/RabbitConfig.java
@@ -0,0 +1,16 @@
+package com.notification.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    public static final String EMAIL_QUEUE = "booking.email.queue";
+
+    @Bean
+    public Queue emailQueue() {
+        return new Queue(EMAIL_QUEUE, true);
+    }
+}
+
